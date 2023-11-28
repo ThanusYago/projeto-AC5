91c95b652b796e0eac56a5c7d3988e6753e81b4f
ThanusYago <119354339+ThanusYago@users.noreply.github.com>
2023-11-27 21:24:12 -0300

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, send_from_directory
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return send_from_directory('projeto-AC5', 'index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
